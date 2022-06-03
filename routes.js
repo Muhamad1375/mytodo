4923203ad8a49cecb3fd4029fcd9e7e13ffedaf7
Muhamad1375 <Mohammadali.fallahzade@gmail.com>
2022-06-03 23:40:56 +0430

    27 todo

---
+++ routes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const mysql = require('mysql')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+      res.render('index')
+})
+
+router.post('/add_todo')
